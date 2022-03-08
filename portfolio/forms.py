55ada68823a74bc32b7fd91b5a18148cbe30b3fe
Kpasula <kpasula@unomaha.edu>
2022-03-07 19:17:26 -0600

    added datetime widgets

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import DateTimeInput
+
 from .models import Customer, Stock, Investment, MutualFund
 
 
@@ -12,6 +15,9 @@ class StockForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ('customer', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date',)
+        widgets = {
+            'purchase_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 
 class InvestmentForm(forms.ModelForm):
@@ -19,9 +25,16 @@ class InvestmentForm(forms.ModelForm):
         model = Investment
         fields = ('customer', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value',
                   'recent_date',)
+        widgets = {
+            'acquired_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+            'recent_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 
 class MutualFundForm(forms.ModelForm):
     class Meta:
         model = MutualFund
         fields = ('customer', 'plan', 'investment_amount', 'current_value', 'acquired_date',)
+        widgets = {
+            'acquired_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
